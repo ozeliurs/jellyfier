d9b14088fdd9a7b07ff235121bc12e8319fe6bf3
Ozeliurs <ozeliurs@gmail.com>
2025-01-29 21:59:26 +0100

    fix(scanner/main.py): add missing 'name' field to audio and subtitle channel dictionaries

--- scanner/main.py
+++ scanner/main.py
@@ -45,12 +45,14 @@ def get_file_info(file_path):
                 )
             elif stream["codec_type"] == "audio":
                 audio_channel = {
+                    "name": stream.get("tags", {}).get("title", "unknown"),
                     "channel": stream.get("tags", {}).get("language", "unknown"),
                     "codec": stream.get("codec_name"),
                 }
                 file_info["audio_channels"].append(audio_channel)
             elif stream["codec_type"] == "subtitle":
                 subtitle_channel = {
+                    "name": stream.get("tags", {}).get("title", "unknown"),
                     "subtitle": stream.get("tags", {}).get("language", "unknown"),
                     "codec": stream.get("codec_name"),
                 }
